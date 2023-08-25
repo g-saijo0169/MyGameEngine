11537fb24d189f64b118837648b9342e66f741f8
元葵 西條 <220064@jc-21.jp>
2023-08-25 11:35:15 +0900

    tankgameに似せればいい

--- Controller.cpp
+++ Controller.cpp
@@ -1,1 +1,33 @@
 #include "Controller.h"
+#include "Engine/Input.h"
+
+//コンストラクタ
+Controller::Controller(GameObject* parent)
+    :GameObject(parent, "Controller")
+{
+}
+
+//デストラクタ
+Controller::~Controller()
+{
+}
+
+//初期化
+void Controller::Initialize()
+{
+}
+
+//更新
+void Controller::Update()
+{
+}
+
+//描画
+void Controller::Draw()
+{
+}
+
+//開放
+void Controller::Release()
+{
+}
