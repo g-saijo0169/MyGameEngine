6227f4fc50b075fb26496ca6d21898de8bc13d61
元葵 西條 <220064@jc-21.jp>
2023-10-16 10:08:22 +0900

    Loadわからん

--- Main.cpp
+++ Main.cpp
@@ -204,6 +204,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			break;
 		case ID_MENU_OPEN:
 			OutputDebugString("open FILE");
+			((Stage*)pRootJob->FindObject("Stage"))->Load();
 			break;
 		case ID_MENU_SAVE:
 			OutputDebugString("Save FILE");
